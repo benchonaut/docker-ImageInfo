4a08a49e79f031b5bc02a13c8189d0f0bf009008
aliceminotto <i.m.e@hotmail.it>
2016-07-08 16:45:01 +0100

    solved #1

--- image-info.sh
+++ image-info.sh
@@ -11,6 +11,11 @@ image_name=$1
 layers=`docker history $1 | tail -n +2 | wc -l`
 size=`docker images $1 | tail -n +2 | awk '{print$(NF-1)"_"$NF}'`
 
-info="_${size}/_${layers}_Layers_"
+if [ "${#layers}" -ne "1" ]
+  then
+    info="_${size}/_${layers}_Layers_"
+  else
+    info="_${size}/${layers}_Layers_"
+fi
 
 echo https://img.shields.io/badge/ImageInfo-${info}-blue.svg?style=flat-square
